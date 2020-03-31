bdc07d84cd121c9347a8aae3448ac34c94b86067
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-03-30 21:29:56 -0600

    added js to make forms react to their respective sign up buttons

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// hide & display form for Fun In The Sun destination //
+function showSun(){
+    document.getElementById("fun_in_the_sunForm").style.display = "block";
+}
+function hideSun(){
+    document.getElementById("fun_in_the_sunForm").style.display = "none";
+}
+
+// hide & display form for Mountain Excursion destination //
+
+function showMountain(){
+    document.getElementById("mountain_excursionForm").style.display = "block";
+}
+function hideMountain(){
+    document.getElementById("mountain_excursionForm").style.display = "none";
+}
+
+// hide & display form for Island Getaway destination //
+
+function showIsland(){
+    document.getElementById("island_getawayForm").style.display = "block";
+}
+function hideIsland(){
+    document.getElementById("island_getawayForm").style.display = "none";
+}
